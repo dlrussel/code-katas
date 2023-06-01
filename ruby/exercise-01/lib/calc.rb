8f9f631c7588d5ecbc22f267ff2db5a90ae13533
Diane Russel <dianerussel@rogers.com>
2023-06-01 12:54:39 -0400

    Refactor digit methods to use method missing with a hash to store values

--- ruby/exercise-01/lib/calc.rb
+++ ruby/exercise-01/lib/calc.rb
@@ -1,54 +1,22 @@
 # lib/calc.rb
 
 class Calc
+  VALUES = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  }
+
   def initialize
     @operands = []
     @operator = nil
-  end
-
-  def one
-    @operands << 1
-    @operands.length > 1 ? solve : self
-  end
-
-  def two
-    @operands << 2
-    @operands.length > 1 ? solve : self
-  end
-
-  def three
-    @operands << 3
-    @operands.length > 1 ? solve : self
-  end
-
-  def four
-    @operands << 4
-    @operands.length > 1 ? solve : self
-  end
-
-  def five
-    @operands << 5
-    @operands.length > 1 ? solve : self
-  end
-
-  def six
-    @operands << 6
-    @operands.length > 1 ? solve : self
-  end
-
-  def seven
-    @operands << 7
-    @operands.length > 1 ? solve : self
-  end
-
-  def eight
-    @operands << 8
-    @operands.length > 1 ? solve : self
-  end
-
-  def nine
-    @operands << 9
-    @operands.length > 1 ? solve : self
   end
 
   def plus
@@ -69,6 +37,15 @@ class Calc
   def divided_by
     @operator = :/
     self
+  end
+
+  def method_missing(method_name)
+    if VALUES.keys.include?(method_name)
+      @operands << VALUES[method_name]
+      @operands.length > 1 ? solve : self
+    else
+      super
+    end
   end
 
   def solve
